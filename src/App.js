a18bfda37c6a075f8e01c8c6c17d8bc493f2c3f7
Shanto <rakibulhasansanto25@gmail.com>
2021-03-22 00:09:52 +0600

    lode the json data
    In this section first i add some data in data.json and lode this data into the App.js file .

--- src/App.js
+++ src/App.js
@@ -10,6 +10,13 @@ return <div>
     <Card />
     <Card texttitle="tite 2" textdes="description 2"/>
     <Card texttitle="tite 2" textdes="description 2"/>
+     
+     {/* using this for loded the json data */}
+     <h1 className="heading">Todoo App 2</h1>
+     <Card texttitle={Data[0].title} textdes={Data[0].desc} />
+     <Card texttitle={Data[1].title} textdes={Data[1].desc} />
+     <Card texttitle={Data[2].title} textdes={Data[2].desc} />
+     <Card texttitle={Data[3].title} textdes={Data[3].desc} />
 </div>
 }
 
